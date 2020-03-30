de6f1da0fe250968b9a6460f040be6161637b59b
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-03-30 18:47:25 -0400

    ecommerce project

---
+++ Javascript Week 3/Easy/index.js
@@ -0,0 +1,11 @@
+function exercise(run, swim) {
+    if (run) {
+        console.log("Today's exercise: running");
+    } else if (swim) {
+        console.log("Today's exercise: swimming");
+    } else {
+        console.log("Today is a rest day");
+    }
+}
+var workout = swim,
+console.log(run);
