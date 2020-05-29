9dcdc9e770dce742a002c125a4a8c5080a8eca0a
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-05-29 17:10:40 -0400

    react-ecommerce

---
+++ React-eCommerce/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Products from './products';
+import Nav from './nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
